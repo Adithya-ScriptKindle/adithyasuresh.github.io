61d02efaf284bc24d145e7132fb02f76ef55f1a8
Adithya-ScriptKindle <adisursad@gmail.com>
2024-02-20 11:48:46 -0500

    Added JS File

---
+++ assets/js/counter.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".view-counter");
+async function updateCounter() {
+  let response = await fetch(
+    "https://3h7jxzyhaufoowcjltuk27lrne0rkbnf.lambda-url.us-east-1.on.aws/"
+  );
+  let data = await response.json();
+  counter.innerHTML = `Built with ❤️ Adithya | Visitors: ${data}`;
+}
